ff81a9b2e25df1b0982b429df805278cc87e0adb
Jens Östlund <jostlund@gmail.com>
2018-11-01 09:32:14 +0100

    Add logging to server

--- src/server.js
+++ src/server.js
@@ -14,12 +14,22 @@ const dataFetcher = require('./data-fetcher.js')(client);
 
 const server = express();
 
+const log = (x) => {
+  console.log(JSON.stringify(x, null, 2));
+  return x;
+};
+
 server.use(auth);
 
 server.use(express.static('public'));
 
 server.get('/', (req, res) => res.sendFile(path.join(__dirname, './assets/index.html')));
 
-server.get('/api/prs', (req, res) => dataFetcher.getPRs(config.repos).then((prs) => res.json(prs)));
+server.get('/api/prs', (req, res) =>
+  dataFetcher
+    .getPRs(config.repos)
+    .then(log)
+    .then((prs) => res.json(prs))
+);
 
 module.exports = server;
