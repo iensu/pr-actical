5f0075895bddeb7d40aca9677ab2e4b1c87ec645
Jens Östlund <jostlund@gmail.com>
2018-11-03 08:19:27 +0100

    Cache data on the server
    Cache the data on the server instead of constantly querying the github API.
    Fetch new data at a 5 minute interval.

--- src/server.js
+++ src/server.js
@@ -19,17 +19,26 @@ const log = (x) => {
   return x;
 };
 
+let data = [];
+const fetchInterval = 5 * 60 * 1000;
+
+const updateData = () => {
+  dataFetcher
+    .getPRs(config.repos)
+    .then(log)
+    .then((prs) => (data = prs));
+};
+
 server.use(auth);
 
 server.use(express.static('public'));
 
 server.get('/', (req, res) => res.sendFile(path.join(__dirname, './assets/index.html')));
 
-server.get('/api/prs', (req, res) =>
-  dataFetcher
-    .getPRs(config.repos)
-    .then(log)
-    .then((prs) => res.json(prs))
-);
+server.get('/api/prs', (req, res) => res.json(data));
+
+updateData();
+
+setInterval(updateData, fetchInterval);
 
 module.exports = server;
